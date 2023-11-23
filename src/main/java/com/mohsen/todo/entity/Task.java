e15b2b053c9a5b704fc4ba9ee53a6684f49b8b2f
mohsenre89 <mohsen.rezaei89@yahoo.com>
2023-11-23 22:43:09 +0100

    Added actuator and data jpa dependencies to pom.xml, created Task entity.

---
+++ src/main/java/com/mohsen/todo/entity/Task.java
@@ -0,0 +1,93 @@
+package com.mohsen.todo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Date creationDate;
+    private Status status;
+
+    public enum Status {
+        TODO,
+        IN_PROGRESS,
+        COMPLETED
+    }
+
+    // Constructors, getters, and setters
+
+    public Task() {
+        // Default constructor
+    }
+
+    public Task(String title, String description, Date creationDate, Status status) {
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.status = status;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    // toString method for easy debugging
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", creationDate=" + creationDate +
+                ", status=" + status +
+                '}';
+    }
+}
